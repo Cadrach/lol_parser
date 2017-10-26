1067b372de8397288cbee3dbe334bebe09858899
Rachid <rachid@actived.fr>
2017-10-26 16:02:00 +0200

    Parsing Chunks & Keyframes: Smaller files

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -36,6 +36,14 @@ class MainController extends Controller
             'nextChunkID' => 'I',
             'offset' => 'I',
         ],
+        'chunk' => [
+            'a' => 'v', //float
+            'b' => 'v', //float
+            'c' => 'v', //float
+            'd' => 'v', //float
+            'e' => 'v', //float
+            'f' => 'v', //float
+        ],
     ];
 
     /**
@@ -79,6 +87,8 @@ class MainController extends Controller
 
     public function getIndex(){
         ini_set('memory_limit', -1);
+        error_reporting(E_ERROR & ~E_DEPRECATED);
+
         echo '<pre>';
 
         //File read
@@ -106,21 +116,40 @@ class MainController extends Controller
         //Encryption Key
         $encryptionKey = $this->getEncryptionKey($payloadHeader);
 
-        $chunkHeaders = [];
+        $blockHeaders = [];
         $blockCounts = $payloadHeader['chunkCount'] + $payloadHeader['keyframeCount'];
         for($i=0;$i<$blockCounts;$i++){
-            $chunkHeaders[] = $this->unpack('chunkHeader', fread($h, 17));
+            $blockHeaders[] = $this->unpack('chunkHeader', fread($h, 17));
         }
 
         $chunks = [];
-        foreach($chunkHeaders as $chunkHeader){
-            $chunk = gzdecode($this->decrypt($encryptionKey, fread($h, $chunkHeader['chunkLength'])));
-            if($chunkHeader['chunkID'] >= $payloadHeader['startGameChunkID']){
-                $chunks[] = $chunk;
+        $keyFrames = [];
+        foreach($blockHeaders as $blockHeader){
+            $chunk = gzdecode($this->decrypt($encryptionKey, fread($h, $blockHeader['chunkLength'])));
+
+            if($blockHeader['chunkID'] < $payloadHeader['startGameChunkID']){
+                //ignore before games starts
+                continue;
+            }
+
+            if($blockHeader['chunkType'] == 1){
+                $chunks[] = substr($chunk, 0, 160);
+            }
+            else if($blockHeader['chunkType'] == 2){
+                $keyFrames[] = substr($chunk, 0, 160);
+            }
+            else{
+                throw new \Exception('Unknown BLOCK');
             }
         }
 
-        file_put_contents($file . '.chunks', implode("\n\n***********************************************************************\n\n", $chunks));
+        $test = $this->unpack('chunk', $keyFrames[0]);
+
+        print_r($test);
+
+
+        file_put_contents($file . '.chunks', implode("\n\n****************************************************************************\n\n", $chunks));
+        file_put_contents($file . '.keyframes', implode("\n\n****************************************************************************\n\n", $keyFrames));
 
 //        $chunkContent = $this->unpack('chunkHeader', fread($h, 17));
 //        print_r($chunkContent);
@@ -137,9 +166,14 @@ class MainController extends Controller
 
 //        die();
 //        echo $encryptionKey . "\n";
-        print_r($chunkHeaders[0]);
+        echo count($chunks) . " CHUNKS, " . count($keyFrames) . " KEYFRAMES\n\n";
+        echo "LENGTHS\n";
         print_r($lengths);
+        echo "\n\n\n\n\n\nPAYLOAD HEADER\n";
         print_r($payloadHeader);
+        echo "\n\n\n\n\n\nBLOCKS HEADERS ".count($blockHeaders)."\n";
+        print_r($blockHeaders);
+        echo "\n\n\n\n\n\nMETADATA\n";
         print_r($metadata);
 
 
